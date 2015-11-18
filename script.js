9a53a24726e5eceb158f06e7d55130bd631bc11e
thanatos007 <lmayancela08@gmail.com>
2015-11-18 22:02:08 +0000

    best project

--- script.js
+++ script.js
@@ -17,7 +17,15 @@ $('#submit-message').submit(function (e) {
   e.preventDefault();
 
   /* TODO: send message to server */
-
+  //. is for class and # is for id
+  var user = $('#submit-user').val()
+  var content = $('#submit-content').val()
+  var callback = function () {
+    //do whatever's here when sendMessage completes
+    $('#submit-content').val("")
+  }
+  sendMessage(user, content, callback);
+    
 });
 
 
@@ -26,10 +34,26 @@ $('#submit-message').submit(function (e) {
 window.setInterval(function () {
 
   getMessages(function (messages) {
+    // messages is an array of messages
     // empty message log
     $('.messages').empty();
 
     // TODO: append messages to <ul class="messages">
+    for (var i = 0; i < messages.length; i++) {
+      
+      //message.user
+      //message.content
+      // put thoes things in between li tags
+      
+      
+      //output --> <li> James: uiadvuyaebfvhieabvioebvuoiwbvu </li>
+      $('.messages').append(produceMessage(messages[i].user, messages[i].content));
+    }
+    
   });
 
 }, 300);
+
+function produceMessage(user, content) {
+  return '<li>' + user + ": " + content + '</li>';
+}
